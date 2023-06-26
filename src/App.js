fe65f6d1a3aa0cce989eb35a17a6676b147e94a1
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-06-26 13:57:17 -0400

    Initial Upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import {
+    Home,
+    Products,
+    SingleProduct,
+    Cart,
+    Checkout,
+    Error,
+    About,
+    PrivateRoute,
+    AuthWrapper
+} from './pages'
+
+import { Navbar, Sidebar } from './components'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+      <Router>
+          <Navbar />
+          <Sidebar />
+        <Switch>
+            <Route exact path='/'>
+                <Home />
+            </Route>
+            <Route exact path='/about'>
+                <About />
+            </Route>
+            <Route exact path='/cart'>
+                <Cart />
+            </Route>
+            <Route exact path='/products'>
+                <Products />
+            </Route>
+            <Route exact path='/products/:id' children={<SingleProduct />} />
+            <Route exact path='/checkout'>
+                <Checkout />
+            </Route>
+            <Route path='*'>
+                <Error />
+            </Route>
+        </Switch>
+      </Router>
+  )
 }
 
 export default App
